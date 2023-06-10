95520905fb22ab52bcf90b05cf4bdcd110548523
Anooj A M <134763272+anooj11@users.noreply.github.com>
2023-06-10 15:00:27 +0530

    Add files via upload

---
+++ Arraymax.java
@@ -0,0 +1,30 @@
+package arraysum;
+public class Arraymax
+{
+	public static int arraysum(int [] arr)
+	{
+		int maxsum= arr[0];
+		int sum=arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			if(sum<0)
+			{
+				sum=arr[i];
+			}
+			else
+			{
+				sum=sum+arr[i];
+			}
+			maxsum= Math.max(sum, maxsum);
+		}
+		return maxsum;
+	}
+	public static void main(String args[])
+	{
+		int array[]= {-2,-3,4,-1,-2,1,5,-3};
+		int result= arraysum(array);
+		System.out.println(result);
+	}
+}
+	
+	
